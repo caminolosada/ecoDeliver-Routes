a0d4eeb14edf2340b87c5e715806209629ec2d26
Camino Losada <camino.losada.alonso@gmail.com>
2023-09-24 10:46:02 +0200

    Connect to OSRM Api

--- app/index.ts
+++ app/index.ts
@@ -5,3 +5,10 @@ import chalk from "chalk";
 const debug = createDebug("ecoDeliver-Routes:root");
 
 debug(chalk.greenBright("testing debug"));
+
+const osmrConnection: string = process.env.OSMR_CONNECTION!;
+
+if (!osmrConnection) {
+  debug(chalk.red("Missing environment variables"));
+  process.exit(1);
+}
---
+++ app/useDirections/useDirections.ts
@@ -0,0 +1,1 @@
+export const apiUrl: string = process.env.OSMR_CONNECTION!;
